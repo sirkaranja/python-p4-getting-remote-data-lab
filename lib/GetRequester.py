035a690d0738d86b72ceefc5ffa90b02cf7e012b
daniel <sirkaranja12@gmail.com>
2023-06-28 23:15:14 +0300

    done with lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,30 +1,36 @@
-
-from urllib import response
 import requests
 import json
 
-from lib.testing.get_requester_test import CONVERTED_DATA
-
-
-
-
 class GetRequester:
-    url= 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
-    JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
-    CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
-
-
     def __init__(self, url):
         self.url = url
-
+    
     def get_response_body(self):
         response = requests.get(self.url)
-        assert response.get_response_body()=JSON_STRING
-
+        return response.content
+    
     def load_json(self):
-        # responce_body= self.get_response_body
-        assert response.load_json()== CONVERTED_DATA
-        # data=json.loads(responce_body)
-        # return data
-       
-        
+        response = self.get_response_body()
+        decoded_response = response.decode('utf-8')
+        converted_data = json.loads(decoded_response)
+        return converted_data
+
+class GetRequesterTest():
+    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+    JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
+    CONVERTED_DATA = [
+        {'name': 'Daniel', 'occupation': 'LG Fridge Salesman'},
+        {'name': 'Joe', 'occupation': 'WiFi Fixer'},
+        {'name': 'Avi', 'occupation': 'DJ'},
+        {'name': 'Howard', 'occupation': 'Mountain Legend'}
+    ]
+
+    def test_get_response(self):
+        requester = GetRequester(self.URL)
+        self.assertEqual(requester.get_response_body(), self.JSON_STRING)
+
+    def test_load_json(self):
+        requester = GetRequester(self.URL)
+        self.assertEqual(requester.load_json(), self.CONVERTED_DATA)
+
+    
--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,36 +1,16 @@
-import requests
-import json
+from GetRequester import GetRequester
+class GetRequesterTest:
+    '''Class {Classname} in {modulename}.py'''
+URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
+CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
 
-class GetRequester:
-    def __init__(self, url):
-        self.url = url
-    
-    def get_response_body(self):
-        response = requests.get(self.url)
-        return response.content
-    
-    def load_json(self):
-        response = self.get_response_body()
-        decoded_response = response.decode('utf-8')
-        converted_data = json.loads(decoded_response)
-        return converted_data
+def test_get_response():
+        '''get_response_body function returns response.'''
+        requester = GetRequester(URL)
+        assert(requester.get_response_body() == JSON_STRING)
 
-class GetRequesterTest():
-    URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
-    JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
-    CONVERTED_DATA = [
-        {'name': 'Daniel', 'occupation': 'LG Fridge Salesman'},
-        {'name': 'Joe', 'occupation': 'WiFi Fixer'},
-        {'name': 'Avi', 'occupation': 'DJ'},
-        {'name': 'Howard', 'occupation': 'Mountain Legend'}
-    ]
-
-    def test_get_response(self):
-        requester = GetRequester(self.URL)
-        self.assertEqual(requester.get_response_body(), self.JSON_STRING)
-
-    def test_load_json(self):
-        requester = GetRequester(self.URL)
-        self.assertEqual(requester.load_json(), self.CONVERTED_DATA)
-
-    
+def test_load_json():
+        '''load_json function returns response.'''
+        requester = GetRequester(URL)
+        assert(requester.load_json() == CONVERTED_DATA)
